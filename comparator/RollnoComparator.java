a01116807b27c94015ffb530796686bfca9de780
skattagmail <94499241+skattagmail@users.noreply.github.com>
2024-03-08 09:41:17 +0530

    Add files via upload

---
+++ comparator/RollnoComparator.java
@@ -0,0 +1,17 @@
+package com.java.comparator;
+
+import java.util.Comparator;
+
+public class RollnoComparator implements Comparator{  
+	public int compare(Object o1,Object o2){  
+		Student s1=(Student)o1;  
+		Student s2=(Student)o2;  
+		  
+		if(s1.rollno==s2.rollno)  
+			return 0;  
+		else if(s1.rollno>s2.rollno)  
+			return 1;  
+		else  
+			return -1;  
+	}  
+}
